25976228697a8e8329a441582fddcdafcad247e0
Zar475 <mabuzar.khan@systemsltd.com>
2024-11-22 18:49:34 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to your Flask app running in Docker!"})
+
+@app.route('/about')
+def about():
+    return jsonify({"message": "This is a simple Flask app running inside a Docker container!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
