361deea05fe584d9636d6d8f0433a98f585e8332
zar <zar@gmail.com>
2024-12-20 12:17:01 +0000

    added new branch

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def about():
     return jsonify({"message": "This is a simple Flask app running inside a Docker container!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5007)
